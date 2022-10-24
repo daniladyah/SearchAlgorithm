466fd18bf2b2c62d478e36b68489af0f31a21d79
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-24 16:17:59 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Arrray to be searched
+        int[] arr = new iint [20]
+        //Number of elements in the array
+        iint n;
+        //Get the number of elements to store in the array
+        int i;
+
+        
+            
+        }
+    }
+}
